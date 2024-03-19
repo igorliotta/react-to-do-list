b4c537a9df9b03291cec58f0c02cb44bf0ddfb8b
igorliotta <liottaigor@gmail.com>
2024-03-19 15:58:33 +0100

    Fix: home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./styles/App.css";
+import Image from "./images/bg-iphone.jpeg";
 
 function App() {
   // Inizio useState
@@ -62,9 +63,9 @@ function App() {
     setShowTodos(!showTodos);
   };
 
-  // Questa funzione resetta totalmente la lista di todo al click sul pulsante 
+  // Questa funzione resetta totalmente la lista di todo al click sul pulsante
   const handleDeleteAllTodos = () => {
-    setTodos([]); 
+    setTodos([]);
   };
 
   return (
@@ -72,11 +73,29 @@ function App() {
       {/* Layout iphone */}
       <div className="iphone-x">
         <div className="side">
-          <div className="screen relative flex m-auto">
+          <div
+            className="screen relative flex m-auto"
+            style={{ backgroundImage: `url(${Image})` }}
+          >
             {/* Qui devo strutturare tutto il codice della to do list */}
-            <button className="button-2 m-auto " onClick={handleToggleTodos}>
-              {showTodos ? "Nascondi To do List" : "Create To do List"}
+
+            {/* Button iniziale schermata home */}
+            <button className="BTN m-auto" onClick={handleToggleTodos}>
+              <span className="svgContainer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="20"
+                  width="20"
+                  viewBox="0 0 512 512"
+                  fill="white"
+                >
+                  <path d="M152.1 38.2c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 113C-2.3 103.6-2.3 88.4 7 79s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zm0 160c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 273c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zM224 96c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zM160 416c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-17.7 0-32-14.3-32-32zM48 368a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                </svg>
+              </span>
+              <span className="BG"></span>
             </button>
+            <p className="text-white m-auto text-home">To Do List</p>
+            {/* Fine button schermata home */}
             {showTodos && (
               <>
                 <div className="flex flex-col gap-5 border border-gray-400 rounded p-5 absolute w-48 h-1/2 overflow-scroll top-50 left-50 bg-white font-serif todolist todolist-container">
@@ -132,22 +151,25 @@ function App() {
                   ) : (
                     <>
                       <div className="flex gap-4 items-center justify-between">
-                      <span className="emoji text-center text-3xl">😱</span>
+                        <span className="emoji text-center text-3xl">😱</span>
                         {/* Button reset */}
-                      <button className="noselect mb-3" onClick={handleDeleteAllTodos}>
-                        <span className="text mt-1">Reset</span>
-                        <span className="icon">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                          >
-                            <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
-                          </svg>
-                        </span>
-                      </button>
-                      {/* Fine button reset */}
+                        <button
+                          className="noselect mb-3"
+                          onClick={handleDeleteAllTodos}
+                        >
+                          <span className="text mt-1">Reset</span>
+                          <span className="icon">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="24"
+                              height="24"
+                              viewBox="0 0 24 24"
+                            >
+                              <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                            </svg>
+                          </span>
+                        </button>
+                        {/* Fine button reset */}
                       </div>
                       <ul className="todos">
                         {todos.map((todo, index) => (
@@ -215,13 +237,13 @@ function App() {
             )}
           </div>
           <div className="line"></div>
-  <div className="header">
-    <div className="sensor-1"></div>
-    <div className="sensor-2"></div>
-    <div className="sensor-3"></div>
-  </div>
-  <div className="volume-button"></div>
-  <div className="power-button"></div>
+          <div className="header">
+            <div className="sensor-1"></div>
+            <div className="sensor-2"></div>
+            <div className="sensor-3"></div>
+          </div>
+          <div className="volume-button"></div>
+          <div className="power-button"></div>
         </div>
       </div>
     </>
