45ee05fceaeb9052a3b29b1606b74c5512a17ef1
igorliotta <liottaigor@gmail.com>
2024-03-19 14:13:40 +0100

    Fix- style to do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Image from "./images/bg.jpg";
 import "./styles/App.css";
 
 function App() {
@@ -65,26 +64,32 @@ function App() {
 
   return (
     <>
-      <div
-        className="flex justify-center items-center h-screen bg-bottom relative"
-        style={{ backgroundImage: `url(${Image})` }}
-      >
-        {/* Pulsante per mostrare/nascondere la to-do list */}
-        <button
-          className="cursor-pointer font-semibold overflow-hidden relative top-70 z-100 border border-green-500 group px-8 py-2"
-          onClick={handleToggleTodos}
-        >
-          <span className="relative z-10 text-green-500 group-hover:text-white text-xl duration-500"></span>
-          <span className="absolute w-full h-full bg-green-500 -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
-          <span className="absolute w-full h-full bg-green-500 -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
-          {showTodos ? "Nascondi To do List" : "Mostra To do List"}
-        </button>
-
-        {/* Contenitore per la to-do list */}
-        {showTodos && (
-          <div className="flex flex-col gap-5 border border-gray-400 rounded p-5 shadow-lg absolute top-96 left-50 bg-white font-serif todolist">
-            <div className="card">
-              <form className="flex items-center justify-between">
+      {/* Layout iphone */}
+      <div className="iphone-x">
+        <div className="side">
+          <div className="screen relative flex m-auto">
+            {/* Qui devo strutturare tutto il codice della to do list */}
+            <button className="button-2 m-auto " onClick={handleToggleTodos}>
+              {showTodos ? "Nascondi To do List" : "Create To do List"}
+            </button>
+            {showTodos && (
+              <>
+              <div className="flex flex-col gap-5 border border-gray-400 rounded p-5 absolute w-48 h-1/2 overflow-scroll top-50 left-50 bg-white font-serif todolist todolist-container">
+                <div className="card">
+                <div className="flex p-2 gap-1">
+                  <div className="" onClick={handleToggleTodos}>
+                    <span className="bg-red-500 inline-block center w-4 h-4 rounded-full"></span>
+                  </div>
+                  <div className="circle">
+                    <span className="bg-yellow-500 inline-block center w-4 h-4 rounded-full"></span>
+                  </div>
+                  <div className="circle">
+                    <span className="bg-green-500 box inline-block center w-4 h-4 rounded-full"></span>
+                  </div>
+                </div>
+                </div>
+                <h2 className="text-center">Todo List</h2>
+                <form className="flex items-center justify-between">
                   <input
                     type="text"
                     placeholder="Inserisci Todo"
@@ -93,96 +98,91 @@ function App() {
                     onChange={handleNewTodoChange}
                     className="px-2 py-2 m-2 write-todo"
                   />
-                <button
-                  className="button"
-                  type="button"
-                  onClick={handleAddTodo}
-                >
-                  <span className="button__text text-center"></span>
-                  <span className="button__icon">
-                    <svg
-                      className="svg"
-                      fill="none"
-                      height="24"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      width="24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <line x1="12" x2="12" y1="5" y2="19"></line>
-                      <line x1="5" x2="19" y1="12" y2="12"></line>
-                    </svg>
-                  </span>
-                </button>
-              </form>
-              {todos.length === 0 ? (
-                <p className="m-4">
-                  Nessun todo da visualizzare
-                  <span className="text-2xl animate-spin"> 🤷</span>
-                </p>
-              ) : (
-                <ul className="todos">
-                  {todos.map((todo, index) => (
-                    <li
-                      className="todo flex gap-5 justify-start items-center m-4"
-                      key={index}
-                    >
-                      {editIndex === index ? (
-                        <>
-                          <input
-                            type="text"
-                            value={editedTodo}
-                            onChange={(e) => setEditedTodo(e.target.value)}
-                            className="border border-2 px-2 py-1"
-                          />
-                          <button className="button save flex justify-center">
-                            <i
-                              className="fa-regular fa-bookmark fa-xl"
-                              onClick={(e) => handleSaveEdit(index, e)}
-                            ></i>
-                          </button>
-                          {/* <button
-                            className="bg-lime-950 text-lime-400 border border-lime-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group"
-                            onClick={(e) => handleSaveEdit(index, e)}
-                          >
-                            <span className="bg-lime-400 shadow-lime-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
-                            Save
-                          </button> */}
-                        </>
-                      ) : (
-                        <>
-                          <div className="todolist flex justify-between">
-                            <div className="flex gap-10">
-                              <label className="checkbox-container">
-                                <input type="checkbox" />
-                                <span className="checkmark"></span>
-                              </label>
-                              <p className="items-center mt-1">{todo.text}</p>
+                  <button
+                    className="button"
+                    type="button"
+                    onClick={handleAddTodo}
+                  >
+                    <span className="button__text text-center"></span>
+                    <span className="button__icon">
+                      <svg
+                        className="svg"
+                        fill="none"
+                        height="24"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        width="24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <line x1="12" x2="12" y1="5" y2="19"></line>
+                        <line x1="5" x2="19" y1="12" y2="12"></line>
+                      </svg>
+                    </span>
+                  </button>
+                </form>
+                {todos.length === 0 ? (
+                  <p className="m-4 p-4 text-7xl text-center">
+                    <span className="emoji">🤓</span>
+                  </p>
+                ) : (
+                  <ul className="todos">
+                    {todos.map((todo, index) => (
+                      <li
+                        className="todo flex gap-5 justify-start items-center m-2"
+                        key={index}
+                      >
+                        {editIndex === index ? (
+                          <>
+                            <input
+                              type="text"
+                              value={editedTodo}
+                              onChange={(e) => setEditedTodo(e.target.value)}
+                              className="border border-2 px-2 py-1"
+                            />
+                            <button className="button save flex justify-center">
+                              <i
+                                className="fa-regular fa-bookmark fa-xl"
+                                onClick={(e) => handleSaveEdit(index, e)}
+                              ></i>
+                            </button>
+                          </>
+                        ) : (
+                          <>
+                            <div className="todolist flex justify-between">
+                              <div className="flex gap-10">
+                                <label className="checkbox-container">
+                                  <input type="checkbox" />
+                                  <span className="checkmark"></span>
+                                </label>
+                                <p className="items-center mt-1">{todo.text}</p>
+                              </div>
+                              <div className="flex gap-5">
+                                <button className="button edit flex justify-center">
+                                  <i
+                                    className="fa-regular fa-pen-to-square"
+                                    onClick={(e) => handleEditTodo(index, e)}
+                                  ></i>
+                                </button>
+                                <button className="button delete flex justify-center">
+                                  <i
+                                    className="fa-regular fa-trash-can"
+                                    onClick={(e) => handleTodoDelete(index, e)}
+                                  ></i>
+                                </button>
+                              </div>
                             </div>
-                            <div className="flex gap-5">
-                              <button className="button edit flex justify-center">
-                                <i
-                                  className="fa-regular fa-pen-to-square"
-                                  onClick={(e) => handleEditTodo(index, e)}
-                                ></i>
-                              </button>
-                              <button className="button delete flex justify-center">
-                                <i
-                                  className="fa-regular fa-trash-can"
-                                  onClick={(e) => handleTodoDelete(index, e)}
-                                ></i>
-                              </button>
-                            </div>
-                          </div>
-                        </>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
+                          </>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+              </>
+            )}
           </div>
-        )}
+        </div>
+        
       </div>
     </>
   );
